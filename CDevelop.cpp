da9acede2719243df7967a7703a627243a014b10
YEXIAONAN <yexiaonanair@outlook.com>
2025-05-22 10:51:09 +0800

    添加项目文件。

---
+++ CDevelop.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int sum = 91;
+
+int main() 
+{
+	int sum = 69;
+	std::cout << "你更喜欢哪种姿势？" << std::endl;
+	std::cout << to_string(sum) + "呀！" << std::endl;
+	return 0;
+}
